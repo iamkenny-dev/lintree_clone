9620224369e70a4c85525e8d5c0c21741fa82c33
iamkenny-dev <104598763+iamkenny-dev@users.noreply.github.com>
2022-10-28 01:38:24 +0100

    update

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,11 @@ const Header = () => {
     <header>
         <div className='header-block'>
             <img src="/images/profile-img.jpg" id='profile__img' alt="profile-image" />
-            <h3 id='twitter'><a href='https://twitter.com/iamkenny-dev'>Kenny Codes</a></h3>
+            <h3 id='twitter'><a href='https://twitter.com/iamkenny_co'>Kenny Codes</a></h3>
+        </div>
+        <div>
+         <img className='mobile_icon' src="/images/mobileshare.png" alt="share_icon" />
+         <img className='desktop_icon' src="/images/wideshare.png" alt="share_icon" />
         </div>
     </header>
   )
